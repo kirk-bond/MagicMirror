e2d3d8c041724e6c7710d148857bde238e145d47
kirk-bond <kirk.bond@gmail.com>
2026-07-29 15:05:02 -0400

    shared ical working

--- config/config.js
+++ config/config.js
@@ -51,14 +51,14 @@ let config = {
 		},
 		{
 			module: "calendar",
-			header: "US Holidays",
+			header: "iCal",
 			position: "top_left",
 			config: {
 				calendars: [
 					{
 						fetchInterval: 7 * 24 * 60 * 60 * 1000,
 						symbol: "calendar-check",
-						url: "https://ics.calendarlabs.com/76/mm3137/US_Holidays.ics"
+						url: "http://p147-caldav.icloud.com/published/2/NzIzODMxNTE3MjM4MzE1MeVcsQHJt-BLeVDYGTNOiSS0hIzCfcUUJfApXAzWqkskrNdOTKZ6zWwhBXa3zCq6nGGJPQ1BNzgQKZ0tpTbPt2U"
 					}
 				]
 			}
